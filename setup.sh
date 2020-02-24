3f8dad3de003c0ea3a0173c8cd36f6ed7a3f63e4
krystofer47 <christoph.brosch@outlook.de>
2020-02-24 11:10:13 +0100

    check for root access

--- setup.sh
+++ setup.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
 [[ -d ~/bin ]] || mkdir ~/bin
-
+if [[ $EUID -eq 0 ]]; then
+	echo "This script is running with root access";
+	exit 0;
+else
+	echo "This script is not running with root access";
+	exit 1;
+fi
