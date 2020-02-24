9449e7167e701e4b05599c898f48b90b8c3c1b6f
Christoph Brosch <me@pop-os.localdomain>
2020-02-24 21:00:19 +0100

    added git installer

--- root.sh
+++ root.sh
@@ -3,11 +3,21 @@ echo "Inside the root.sh script";
 
 # install Visual Studeio Code on Ubunut and Debian based distros
 
+echo "Installing Visual Studio Code";
+
 wget https://go.microsoft.com/fwlink/?LinkID=760868
 cp index.html/?LinkID=760868 code.deb
 rm index.html/?LinkID=760868 -f
 sudo apt install code.deb
 
+# Check if package git is installed
+
+echo "Installing Git";
+sudo apt-get install git
+
 # setup 'Snippet Daemon' for Visual Studio Code
+
+cd ~/.config/Code/User
+git clone https://github.com/rahondev/vsc_snippets.git
 
 exit 0;
