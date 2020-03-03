d7ba23fa05fe6150f8c37f9e4f4f1c5f6b8864d0
Christoph Brosch <44674265+rahondev@users.noreply.github.com>
2020-03-03 20:23:02 +0100

    Update root.sh

--- root.sh
+++ root.sh
@@ -8,7 +8,7 @@ echo "Installing Visual Studio Code";
 wget https://go.microsoft.com/fwlink/?LinkID=760868
 cp index.html\?LinkID=760868 code.deb
 rm index.html\?LinkID=760868 -f
-sudo apt install ./code.deb
+sudo apt install ./code.deb <<< y
 
 # Check if package git is installed
 
